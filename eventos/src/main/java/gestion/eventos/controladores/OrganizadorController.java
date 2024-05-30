d3378927a13fdd387034b812c70afc38cb35759f
dajh904 <dajh904@gmail.com>
2024-05-30 18:59:59 -0500

    Registro organizador

--- eventos/src/main/java/gestion/eventos/controladores/OrganizadorController.java
+++ eventos/src/main/java/gestion/eventos/controladores/OrganizadorController.java
@@ -1,5 +1,36 @@
 package gestion.eventos.controladores;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import gestion.eventos.entidades.Organizador;
+import gestion.eventos.repositorios.OrganizadorRepository;
+
+@Controller
 public class OrganizadorController {
+    @Autowired
+    private OrganizadorRepository organizadorRepo;
+
+    @GetMapping("/registro")
+    public String regOrganizador(Model model){
+        Organizador org = new Organizador();
+        model.addAttribute("organizador", org);
+        return "registro_organizador";
+    }
+
+    @PostMapping("/registro/guardar")
+    public String guardarOrganizador(@ModelAttribute Organizador organizador){
+        try{
+            organizadorRepo.save(organizador);
+        } catch (Exception e){
+            e.printStackTrace();
+            return "redirect:/registro?error";
+        }
+        return "redirect:/login?registro";
+    }
 
 }
--- eventos/src/main/java/gestion/eventos/controladores/Tipoboleto_eventoController.java
+++ eventos/src/main/java/gestion/eventos/controladores/Tipoboleto_eventoController.java
@@ -14,7 +14,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
-import gestion.eventos.entidades.Evento;
 import gestion.eventos.entidades.Imagenevento;
 import gestion.eventos.entidades.Tipoboleto_evento;
 import gestion.eventos.repositorios.ImageneventoRepository;
